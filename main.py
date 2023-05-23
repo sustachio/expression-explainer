c4c0a8b87678d996dd70ad33d06387a781ff376c
sustachio <sustachio@gmail.com>
2023-05-23 18:55:06 -0500

    Style: add info about steps

--- main.py
+++ main.py
@@ -39,26 +39,26 @@ image.show()
        1.2.2 A variable: get its value, and push onto the value stack.
        1.2.3 A left parenthesis: push it onto the operator stack.
        1.2.4 A right parenthesis:
-         1 While the thing on top of the operator stack is not a 
+         1.2.4.1 While the thing on top of the operator stack is not a 
            left parenthesis,
-             1 Pop the operator from the operator stack.
-             2 Pop the value stack twice, getting two operands.
-             3 Apply the operator to the operands, in the correct order.
-             4 Push the result onto the value stack.
-         2 Pop the left parenthesis from the operator stack, and discard it.
+             1.2.4.1.1 Pop the operator from the operator stack.
+             1.2.4.1.2 Pop the value stack twice, getting two operands.
+             1.2.4.1.3 Apply the operator to the operands, in the correct order.
+             1.2.4.1.4 Push the result onto the value stack.
+         1.2.4.2 Pop the left parenthesis from the operator stack, and discard it.
        1.2.5 An operator (call it thisOp):
-         1 While the operator stack is not empty, and the top thing on the
+         1.2.5.1 While the operator stack is not empty, and the top thing on the
            operator stack has the same or greater precedence as thisOp,
-           1 Pop the operator from the operator stack.
-           2 Pop the value stack twice, getting two operands.
-           3 Apply the operator to the operands, in the correct order.
-           4 Push the result onto the value stack.
-         2 Push thisOp onto the operator stack.
+           1.2.5.1.1 Pop the operator from the operator stack.
+           1.2.5.1.2 Pop the value stack twice, getting two operands.
+           1.2.5.1.3 Apply the operator to the operands, in the correct order.
+           1.2.5.1.4 Push the result onto the value stack.
+         1.2.5.2 Push thisOp onto the operator stack.
 2. While the operator stack is not empty,
-    1 Pop the operator from the operator stack.
-    2 Pop the value stack twice, getting two operands.
-    3 Apply the operator to the operands, in the correct order.
-    4 Push the result onto the value stack.
+    2.1 Pop the operator from the operator stack.
+    2.2 Pop the value stack twice, getting two operands.
+    2.3 Apply the operator to the operands, in the correct order.
+    2.4 Push the result onto the value stack.
 3. At this point the operator stack should be empty, and the value
    stack should have only one value in it, which is the final result.
 """
@@ -100,10 +100,14 @@ def algorithm(expression: str) -> int | float:
     index = 0
 
 
+    # 1. While there are still tokens to be read in,
     while index < len(expression):
-        print(operator_stack, value_stack)
+        # 1.1 Get the next token.
         token = expression[index]
 
+        # 1.2 If the token is:
+
+        # 1.2.1 A number: push it onto the value stack.
         if token.isdigit():
             number = ""
 
@@ -117,46 +121,75 @@ def algorithm(expression: str) -> int | float:
 
             continue
 
+
+        # 1.2.2 A variable: get its value, and push onto the value stack.
         # TODO: implement variables
 
 
+        # 1.2.3 A left parenthesis: push it onto the operator stack.
         elif token == "(":
             operator_stack.append(token)
 
 
+        # 1.2.4 A right parenthesis:
         elif token == ")":
-            # go to the last (
+
+            # 1.2.4.1 While the thing on top of the operator stack is not a left parenthesis,
             while operator_stack[-1] != "(":
+
+                # 1.2.4.1.1 Pop the operator from the operator stack.
                 operator = operator_stack.pop()
+                # 1.2.4.1.2 Pop the value stack twice, getting two operands.
                 right, left = value_stack.pop(), value_stack.pop()
 
-                value_stack.append(calculate(operator, left, right))
+                # 1.2.4.1.3 Apply the operator to the operands, in the correct order.
+                result = calculate(operator, left, right)
 
-            operator_stack.pop() # the (
+                # 1.2.4.1.4 Push the result onto the value stack.
+                value_stack.append(result)
+
+            # 1.2.4.2 Pop the left parenthesis from the operator stack, and discard it.
+            operator_stack.pop()
 
 
+        # 1.2.5 An operator:
         elif token in "+-*/":
 
+            # 1.2.5.1 While the operator stack is not empty, and the top thing on the operator stack has the same or greater precedence as this operator,
             while len(operator_stack) and get_predecence(operator_stack[-1]) >= get_predecence(token):
+                # 1.2.5.1.1 Pop the operator from the operator stack.
                 operator = operator_stack.pop()
+                # 1.2.5.1.2 Pop the value stack twice, getting two operands.
                 right, left = value_stack.pop(), value_stack.pop()
 
-                value_stack.append(calculate(operator, left, right))
+                # 1.2.5.1.3 Apply the operator to the operands, in the correct order.
+                result = calculate(operator, left, right)
 
+                # 1.2.5.1.4 Push the result onto the value stack.
+                value_stack.append(result)
+
+            # 1.2.5.2 Push thisOp onto the operator stack.
             operator_stack.append(token)
 
         index += 1
 
 
 
-
+    # 2. While the operator stack is not empty,
     while len(operator_stack):
+        # 2.1 Pop the operator from the operator stack.
         operator = operator_stack.pop()
+        # 2.2 Pop the value stack twice, getting two operands.
         right, left = value_stack.pop(), value_stack.pop()
 
-        value_stack.append(calculate(operator, left, right))
+        # 2.3 Apply the operator to the operands, in the correct order.
+        result = calculate(operator, left, right)
+
+        # 2.4 Push the result onto the value stack.
+        value_stack.append(result)
 
 
+    # 3. At this point the operator stack should be empty, and the value stack should have only one value in it, which is the final result.
     return value_stack[0]
 
 
